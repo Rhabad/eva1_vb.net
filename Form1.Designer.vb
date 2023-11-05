5ca49fe0cea80ba52966d46ad5474f5b326f768d
Rhabad <nicolaspizarro.450@gmail.com>
2023-11-04 22:36:12 -0300

    eva 1

---
+++ Form1.Designer.vb
@@ -0,0 +1,285 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        lblRut = New Label()
+        lblNombre = New Label()
+        lblApellido = New Label()
+        lblCorreo = New Label()
+        lblDireccion = New Label()
+        lbTitulo = New Label()
+        txtRut = New TextBox()
+        txtNombre = New TextBox()
+        txtApellido = New TextBox()
+        txtCorreo = New TextBox()
+        lblEdad = New Label()
+        lblGenero = New Label()
+        txtDireccion = New TextBox()
+        rbMasculino = New RadioButton()
+        rbFemenino = New RadioButton()
+        numEdad = New NumericUpDown()
+        btnAceptar = New Button()
+        tbox = New TextBox()
+        btnBorrar = New Button()
+        btnMostrar = New Button()
+        CType(numEdad, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' lblRut
+        ' 
+        lblRut.AutoSize = True
+        lblRut.Location = New Point(72, 127)
+        lblRut.Name = "lblRut"
+        lblRut.Size = New Size(25, 15)
+        lblRut.TabIndex = 0
+        lblRut.Text = "Rut"
+        ' 
+        ' lblNombre
+        ' 
+        lblNombre.AutoSize = True
+        lblNombre.Location = New Point(46, 157)
+        lblNombre.Name = "lblNombre"
+        lblNombre.Size = New Size(51, 15)
+        lblNombre.TabIndex = 1
+        lblNombre.Text = "Nombre"
+        ' 
+        ' lblApellido
+        ' 
+        lblApellido.AutoSize = True
+        lblApellido.Location = New Point(46, 187)
+        lblApellido.Name = "lblApellido"
+        lblApellido.Size = New Size(51, 15)
+        lblApellido.TabIndex = 2
+        lblApellido.Text = "Apellido"
+        ' 
+        ' lblCorreo
+        ' 
+        lblCorreo.AutoSize = True
+        lblCorreo.Location = New Point(45, 277)
+        lblCorreo.Name = "lblCorreo"
+        lblCorreo.Size = New Size(43, 15)
+        lblCorreo.TabIndex = 3
+        lblCorreo.Text = "Correo"
+        ' 
+        ' lblDireccion
+        ' 
+        lblDireccion.AutoSize = True
+        lblDireccion.Location = New Point(40, 307)
+        lblDireccion.Name = "lblDireccion"
+        lblDireccion.Size = New Size(57, 15)
+        lblDireccion.TabIndex = 4
+        lblDireccion.Text = "Direccion"
+        ' 
+        ' lbTitulo
+        ' 
+        lbTitulo.AutoSize = True
+        lbTitulo.Font = New Font("Showcard Gothic", 18F, FontStyle.Italic, GraphicsUnit.Point)
+        lbTitulo.ForeColor = Color.Lime
+        lbTitulo.Location = New Point(146, 44)
+        lbTitulo.Name = "lbTitulo"
+        lbTitulo.Size = New Size(304, 30)
+        lbTitulo.TabIndex = 6
+        lbTitulo.Text = "Registro De Empleados"
+        ' 
+        ' txtRut
+        ' 
+        txtRut.Location = New Point(116, 119)
+        txtRut.Name = "txtRut"
+        txtRut.Size = New Size(142, 23)
+        txtRut.TabIndex = 7
+        ' 
+        ' txtNombre
+        ' 
+        txtNombre.Location = New Point(116, 154)
+        txtNombre.Name = "txtNombre"
+        txtNombre.Size = New Size(142, 23)
+        txtNombre.TabIndex = 8
+        ' 
+        ' txtApellido
+        ' 
+        txtApellido.Location = New Point(116, 183)
+        txtApellido.Name = "txtApellido"
+        txtApellido.Size = New Size(142, 23)
+        txtApellido.TabIndex = 9
+        ' 
+        ' txtCorreo
+        ' 
+        txtCorreo.Location = New Point(116, 277)
+        txtCorreo.Name = "txtCorreo"
+        txtCorreo.Size = New Size(142, 23)
+        txtCorreo.TabIndex = 11
+        ' 
+        ' lblEdad
+        ' 
+        lblEdad.AutoSize = True
+        lblEdad.Location = New Point(64, 217)
+        lblEdad.Name = "lblEdad"
+        lblEdad.Size = New Size(33, 15)
+        lblEdad.TabIndex = 12
+        lblEdad.Text = "Edad"
+        ' 
+        ' lblGenero
+        ' 
+        lblGenero.AutoSize = True
+        lblGenero.Location = New Point(52, 247)
+        lblGenero.Name = "lblGenero"
+        lblGenero.Size = New Size(45, 15)
+        lblGenero.TabIndex = 13
+        lblGenero.Text = "Genero"
+        ' 
+        ' txtDireccion
+        ' 
+        txtDireccion.Location = New Point(116, 307)
+        txtDireccion.Name = "txtDireccion"
+        txtDireccion.Size = New Size(142, 23)
+        txtDireccion.TabIndex = 14
+        ' 
+        ' rbMasculino
+        ' 
+        rbMasculino.AutoSize = True
+        rbMasculino.Location = New Point(116, 245)
+        rbMasculino.Name = "rbMasculino"
+        rbMasculino.Size = New Size(80, 19)
+        rbMasculino.TabIndex = 15
+        rbMasculino.TabStop = True
+        rbMasculino.Text = "Masculino"
+        rbMasculino.UseVisualStyleBackColor = True
+        ' 
+        ' rbFemenino
+        ' 
+        rbFemenino.AutoSize = True
+        rbFemenino.Location = New Point(202, 245)
+        rbFemenino.Name = "rbFemenino"
+        rbFemenino.Size = New Size(78, 19)
+        rbFemenino.TabIndex = 16
+        rbFemenino.TabStop = True
+        rbFemenino.Text = "Femenino"
+        rbFemenino.UseVisualStyleBackColor = True
+        ' 
+        ' numEdad
+        ' 
+        numEdad.Location = New Point(116, 215)
+        numEdad.Maximum = New Decimal(New Integer() {65, 0, 0, 0})
+        numEdad.Minimum = New Decimal(New Integer() {18, 0, 0, 0})
+        numEdad.Name = "numEdad"
+        numEdad.Size = New Size(39, 23)
+        numEdad.TabIndex = 17
+        numEdad.Value = New Decimal(New Integer() {18, 0, 0, 0})
+        ' 
+        ' btnAceptar
+        ' 
+        btnAceptar.BackColor = Color.FromArgb(CByte(128), CByte(128), CByte(255))
+        btnAceptar.ForeColor = Color.Yellow
+        btnAceptar.Location = New Point(12, 346)
+        btnAceptar.Name = "btnAceptar"
+        btnAceptar.Size = New Size(85, 45)
+        btnAceptar.TabIndex = 18
+        btnAceptar.Text = "Aceptar"
+        btnAceptar.UseVisualStyleBackColor = False
+        ' 
+        ' tbox
+        ' 
+        tbox.Location = New Point(298, 119)
+        tbox.Multiline = True
+        tbox.Name = "tbox"
+        tbox.ReadOnly = True
+        tbox.ScrollBars = ScrollBars.Vertical
+        tbox.Size = New Size(261, 272)
+        tbox.TabIndex = 19
+        ' 
+        ' btnBorrar
+        ' 
+        btnBorrar.BackColor = Color.FromArgb(CByte(128), CByte(128), CByte(255))
+        btnBorrar.ForeColor = Color.Yellow
+        btnBorrar.Location = New Point(103, 346)
+        btnBorrar.Name = "btnBorrar"
+        btnBorrar.Size = New Size(85, 45)
+        btnBorrar.TabIndex = 20
+        btnBorrar.Text = "Borrar"
+        btnBorrar.UseVisualStyleBackColor = False
+        ' 
+        ' btnMostrar
+        ' 
+        btnMostrar.BackColor = Color.FromArgb(CByte(128), CByte(128), CByte(255))
+        btnMostrar.ForeColor = Color.Yellow
+        btnMostrar.Location = New Point(195, 346)
+        btnMostrar.Name = "btnMostrar"
+        btnMostrar.Size = New Size(85, 45)
+        btnMostrar.TabIndex = 21
+        btnMostrar.Text = "Mostrar"
+        btnMostrar.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        ClientSize = New Size(571, 423)
+        Controls.Add(btnMostrar)
+        Controls.Add(btnBorrar)
+        Controls.Add(tbox)
+        Controls.Add(btnAceptar)
+        Controls.Add(numEdad)
+        Controls.Add(rbFemenino)
+        Controls.Add(rbMasculino)
+        Controls.Add(txtDireccion)
+        Controls.Add(lblGenero)
+        Controls.Add(lblEdad)
+        Controls.Add(txtCorreo)
+        Controls.Add(txtApellido)
+        Controls.Add(txtNombre)
+        Controls.Add(txtRut)
+        Controls.Add(lbTitulo)
+        Controls.Add(lblDireccion)
+        Controls.Add(lblCorreo)
+        Controls.Add(lblApellido)
+        Controls.Add(lblNombre)
+        Controls.Add(lblRut)
+        Name = "Form1"
+        Text = "Form1"
+        CType(numEdad, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lblRut As Label
+    Friend WithEvents lblNombre As Label
+    Friend WithEvents lblApellido As Label
+    Friend WithEvents lblCorreo As Label
+    Friend WithEvents lblDireccion As Label
+    Friend WithEvents lbTitulo As Label
+    Friend WithEvents txtRut As TextBox
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtApellido As TextBox
+    Friend WithEvents txtCorreo As TextBox
+    Friend WithEvents lblEdad As Label
+    Friend WithEvents lblGenero As Label
+    Friend WithEvents txtDireccion As TextBox
+    Friend WithEvents rbMasculino As RadioButton
+    Friend WithEvents rbFemenino As RadioButton
+    Friend WithEvents numEdad As NumericUpDown
+    Friend WithEvents btnAceptar As Button
+    Friend WithEvents tbox As TextBox
+    Friend WithEvents btnBorrar As Button
+    Friend WithEvents btnMostrar As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,43 @@
+﻿Imports System.Text
+Imports MySql.Data.MySqlClient
+Public Class Form1
+
+    Private cadenaConexion As String = "Server=localhost;Database=eva1_vb;User Id=root;Password=1234;"
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+    End Sub
+
+    Private Sub btnMostrar_Click(sender As Object, e As EventArgs) Handles btnMostrar.Click
+        Dim query As String = "select * from cliente;"
+        Try
+            Using connection As New MySqlConnection(cadenaConexion)
+                Using cmd As New MySqlCommand(query, connection)
+                    connection.Open()
+
+                    Dim lector As MySqlDataReader = cmd.ExecuteReader()
+
+                    Dim resultado As New StringBuilder
+
+                    While lector.Read()
+                        resultado.AppendLine("Rut: " & lector("rut").ToString)
+                        resultado.AppendLine("Nombre: " & lector("nombre").ToString)
+                        resultado.AppendLine("Apellido: " & lector("apellido").ToString)
+                        resultado.AppendLine("Edad: " & lector("edad").ToString)
+                        resultado.AppendLine("Genero: " & lector("genero").ToString)
+                        resultado.AppendLine("Correo: " & lector("correo").ToString)
+                        resultado.AppendLine("Direccion: " & lector("direccion").ToString)
+
+                        resultado.AppendLine()
+                    End While
+
+                    tbox.Text = resultado.ToString
+                End Using
+            End Using
+        Catch ex As Exception
+            MsgBox("no se pudo conectar" & ex.Message)
+        End Try
+    End Sub
+End Class
